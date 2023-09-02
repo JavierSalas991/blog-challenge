8da6d68ea9389369e96f2335e8c6e94efc6eb519
Javier salas <49585.Git>
2023-09-02 09:58:59 -0300

    ok

--- blog/src/components/posts/PostDetail.js
+++ blog/src/components/posts/PostDetail.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import { getPost } from '../../helpers/apiHHelper';
+import { useParams, useNavigate } from 'react-router';
+import { getPost, getUser } from '../../helpers/apiHHelper';
 import Error404 from '../Error404';
 import Loading from '../Loading';
 import { spanishDate } from '../../helpers/helper';
 
 const PostDetail = () => {
+
+    const navigate = useNavigate()
+
     const params = useParams();
     const id = params.id || null
 
     const [postDetails, setPostDetails] = useState(null)
     const [error, setError] = useState(false)
+    const [author, setAuthor] = useState(null)
 
     const getPostById = async id => {
         try {
@@ -23,12 +27,30 @@ const PostDetail = () => {
         }
     }
 
+    const getAuthor = async id => {
+        getUser(id)
+            .then(res => {
+                res && res.data && setAuthor(res.data)
+            })
+            .catch(error => console.log(error))
+    }
+
     useEffect(() => {
         id && getPostById(id)
     }, [id])
 
+    useEffect(() => {
+        postDetails && getAuthor(postDetails.author_id)
+    }, [postDetails])
+
+    useEffect(() => {
+      console.log('====================================');
+      console.log(author);
+      console.log('====================================');
+    }, [author])
+
     return (
-        postDetails ?
+        postDetails && author ?
             <div style={{ marginTop: "5rem" }}>
                 <div className='row'>
                     <div className='col-8 m-4'>
@@ -39,7 +61,8 @@ const PostDetail = () => {
                         <p
                             style={{ fontSize: "85%", marginLeft: "" }}
                             className='my-0 text-muted'>
-                            Publicado el dia {spanishDate(postDetails.created_at)}
+                            Publicado el dia {spanishDate(postDetails.created_at)} por 
+                            <span onClick={() => navigate("/profile/"+author.id)} style={{cursor: "pointer"}} className='text-primary'> {author.name}</span>
                         </p>
                     </div>
                     <div className='col-4'>
